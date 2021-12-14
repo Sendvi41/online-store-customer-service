c0ad64c15c3cb69a9a4bda72abd5d6462a0e719a
Semen Druzhinin <Semen_Druzhinin@epam.com>
2021-12-14 10:19:27 +0300

    Initial commit

---
+++ src/test/java/com/epam/druzhinin/OnlineStoreCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.druzhinin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineStoreCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
