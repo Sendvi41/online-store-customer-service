36e74b7c0d6ab951cd1d9f8d188f7db8ba7de5ab
Semen Druzhinin <Semen_Druzhinin@epam.com>
2021-12-14 17:50:24 +0300

    Added elasticsearch

---
+++ src/main/java/com/epam/druzhinin/config/ElasticsearchClientConfig.java
@@ -0,0 +1,33 @@
+package com.epam.druzhinin.config;
+
+import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
+
+@Configuration
+@EnableElasticsearchRepositories(basePackages
+        = "com.epam.druzhinin.repositories")
+@ComponentScan(basePackages = {"com.epam.druzhinin"})
+public class ElasticsearchClientConfig extends AbstractElasticsearchConfiguration {
+    @Value("${elastic.host.and.port}")
+    private String elasticHostAndPort;
+
+    @Override
+    @Bean
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration =
+                ClientConfiguration
+                        .builder()
+                        .connectedTo(elasticHostAndPort)
+                        .build();
+
+        return RestClients.create(clientConfiguration).rest();
+    }
+}
+
