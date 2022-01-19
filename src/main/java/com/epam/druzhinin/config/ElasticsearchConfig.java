904ab0899a4de36a3ab226192b4cf9e0ff74ba68
Semen Druzhinin <Semen_Druzhinin@epam.com>
2022-01-19 17:40:36 +0300

    Fixed ElasticsearchConfig

--- src/main/java/com/epam/druzhinin/config/ElasticsearchConfig.java
+++ src/main/java/com/epam/druzhinin/config/ElasticsearchConfig.java
@@ -1,20 +1,19 @@
 package com.epam.druzhinin.config;
 
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
-import org.springframework.data.elasticsearch.core.ElasticsearchRestTemplate;
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.RestClients;
+import org.springframework.data.elasticsearch.config.AbstractElasticsearchConfiguration;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @Configuration
 @EnableElasticsearchRepositories(basePackages = "com.epam.druzhinin.repositories")
 @ComponentScan(basePackages = {"com.epam.druzhinin"})
-public class ElasticsearchConfig {
+public class ElasticsearchConfig extends AbstractElasticsearchConfiguration {
 
     @Value("${elastic.host}")
     private String elasticHost;
@@ -22,15 +21,15 @@ public class ElasticsearchConfig {
     @Value("${elastic.port}")
     private String elasticPort;
 
-    @Bean(destroyMethod = "close")
-    public RestHighLevelClient client() {
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(new HttpHost(elasticHost, Integer.parseInt(elasticPort), "https")));
-        return client;
-    }
-
+    @Override
     @Bean
-    public ElasticsearchOperations elasticsearchTemplate() {
-        return new ElasticsearchRestTemplate(client());
+    public RestHighLevelClient elasticsearchClient() {
+        ClientConfiguration clientConfiguration =
+                ClientConfiguration
+                        .builder()
+                        .connectedTo(elasticHost + ":" + elasticPort)
+                        .build();
+
+        return RestClients.create(clientConfiguration).rest();
     }
 }
